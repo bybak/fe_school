ca080799223ef9c46448eed9cf10f37ff14a676a
Andrey <bybacha@mail.ru>
2019-03-21 03:25:01 +0300

    profile page

--- vue/films/src/components/Profile.vue
+++ vue/films/src/components/Profile.vue
@@ -34,51 +34,57 @@
       </b-col>
     </b-form-row>
 
+    <b-card class="mt-3" bg-variant="dark" text-variant="light">
+
+      <b-form-row>
+        <b-col lg="3" cols="12">
+          <b-button block variant="secondary" class="text-left">Friends</b-button>
+          <b-button block variant="secondary" class="d-flex justify-content-between align-items-center">Requests <b-badge variant="light">4</b-badge></b-button>
+        </b-col>
+        <b-col lg="9" sm="12">
+
+          <b-form-row>
+            <b-col lg="1" cols="3">
+              <img class="rounded" src="../../pictures/pic/1.jpg" alt="Card image" style="border-radius: 3px; max-height: 70px;">
+            </b-col>
+            <b-col lg="11" cols="9">
+              <div class="d-flex justify-content-between align-items-start">
+                <div>
+                  <div><span class="font-weight-bold">User Name</span></div>
+                  <div><span>From: 01.01.2019</span></div>
+                </div>
+                <b-button variant="danger" size="sm" v-b-tooltip.hover title="Remove from friends"><i class="fas fa-times"></i></b-button>
+              </div>
+            </b-col>
+          </b-form-row>
+
+          <hr>
+
+          <b-form-row>
+            <b-col lg="1" cols="3">
+              <img class="rounded" src="../../pictures/pic/2.jpg" alt="Card image" style="border-radius: 3px; max-height: 70px;">
+            </b-col>
+            <b-col lg="11" cols="9">
+              <div class="d-flex justify-content-between align-items-start">
+                <div>
+                  <div><span class="font-weight-bold">User Name</span></div>
+                  <div><span>From: 01.01.2019</span></div>
+                </div>
+                <div>
+                  <b-button variant="primary" size="sm" v-b-tooltip.hover title="Remove from friends"><i class="fas fa-film"></i></b-button>
+                  <b-button variant="danger" size="sm" v-b-tooltip.hover title="Remove from friends" class="ml-1"><i class="fas fa-times"></i></b-button>
+                </div>
+              </div>
+            </b-col>
+          </b-form-row>
+
+        </b-col>
+      </b-form-row>
+
+    </b-card>
+
     <b-form-row class="mt-3">
-      <b-col lg="6">
-        <b-card
-                style=""
-                class="mb-2"
-        >
-          <b-card-title>
-            Friends <small> (They can view your collection)</small>
-          </b-card-title>
-          <b-card-text>
-            <b-list-group flush>
-
-              <b-list-group-item class="d-flex justify-content-between align-items-center">
-                <div class="text-center">
-                  <i class="fas fa-user fa-3x"></i>
-                  <br>
-                  Your friend name
-                </div>
-                <div>
-                  <b-button-group vertical>
-                    <b-button variant="primary">View user's collection</b-button>
-                    <b-button variant="danger">Remove from friends</b-button>
-                  </b-button-group>
-                </div>
-              </b-list-group-item>
-
-              <b-list-group-item class="d-flex justify-content-between align-items-center">
-                <div class="text-center">
-                  <i class="fas fa-user fa-3x"></i>
-                  <br>
-                  Your friend name
-                </div>
-                <div>
-                  <b-button-group vertical>
-                    <b-button variant="primary">View user's collection</b-button>
-                    <b-button variant="danger">Remove from friends</b-button>
-                  </b-button-group>
-                </div>
-              </b-list-group-item>
-
-            </b-list-group>
-          </b-card-text>
-
-        </b-card>
-      </b-col>
+      
       <b-col lg="6">
 
         <b-card
@@ -146,14 +152,23 @@ export default {
   name: 'profile',
   data() {
       return {
-
+          tabIndex: 0,
       }
   },
   methods: {
-
+      linkClass(idx) {
+          if (this.tabIndex === idx) {
+              return ['bg-secondary', 'text-light']
+          } else {
+              return ['text-light']
+          }
+      }
   }
 }
 </script>
 
 <style>
+  .tab-pane:focus {
+    outline: none;
+  }
 </style>
